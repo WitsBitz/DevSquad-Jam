b493881a101a24613eee788b89c81a6eaf417621
Roldan Melcon <roldan@thecosmicwhale.com>
2017-07-01 01:20:58 -0700

    merge and add input testing

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 touchPos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad, OVRInput.Controller.RTrackedRemote);
+
+		if(touchPos.y > 0)
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime);
+		}
+
+	}
+}
