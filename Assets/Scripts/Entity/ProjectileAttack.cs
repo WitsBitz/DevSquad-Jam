7f22bf87cac9c47445d86f7e061b4da56fba975a
alexnaraghi <alexandernaraghi@gmail.com>
2017-07-01 12:20:23 -0700

    Made enemy combat more interesting.

--- Assets/Scripts/Entity/ProjectileAttack.cs
+++ Assets/Scripts/Entity/ProjectileAttack.cs
@@ -6,45 +6,12 @@ using UnityEngine;
 public class ProjectileAttack : MonoBehaviour 
 {
     [SerializeField] private GameObject attackOrigin;
-    [SerializeField] private float rotationSpeed;
-    private GameObject target;
-
-    // Weapon details
     [SerializeField] private GameObject projectilePrefab;
-    [SerializeField] private float cooldown;
-    private float currentCooldown;
 
-	private void Update () 
-    {
-        // Try to find the evil human
-        if(target == null)
-        {
-            target = Game.instance.player;
-        }
-
-        TurnTowardsTarget(target);
-        ShootAt(target);
-    }
-
-    private void TurnTowardsTarget(GameObject target)
+    public void ShootAt(GameObject target)
     {
         if(target != null)
         {
-            Vector3 targetDir = target.transform.position - transform.position;
-            float step = rotationSpeed * Time.deltaTime;
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0f);
-            transform.rotation = Quaternion.LookRotation(newDir);
-        }
-    }
-
-
-    private void ShootAt(GameObject target)
-    {
-        currentCooldown -= Time.deltaTime;
-        if(target != null && currentCooldown <= 0)
-        {
-            currentCooldown = cooldown;
-
             if(projectilePrefab != null && attackOrigin != null)
             {
                 var projectileObj = Instantiate(projectilePrefab, 
